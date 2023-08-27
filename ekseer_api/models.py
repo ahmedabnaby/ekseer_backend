59f8e673436ebc0121de7018489926c2e1f2899c
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2023-08-27 18:13:49 +0300

    trying to push models with new forget password url

--- ekseer_api/models.py
+++ ekseer_api/models.py
@@ -139,7 +139,7 @@ class Call(models.Model):
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
     # the below like concatinates your websites reset password url and the reset email token which will be required at a later stage
-    email_plaintext_message = "Open the link to reset your password" + " " + "{}{}".format(instance.request.build_absolute_uri("https://aabdelnaby.com/forget-password-form/"), reset_password_token.key)
+    email_plaintext_message = "Open the link to reset your password" + " " + "{}{}".format(instance.request.build_absolute_uri("http://localhost:3000/"), reset_password_token.key)
     
     """
         this below line is the django default sending email function, 
