7770c73b030be2072c9809512d8c3cf4ae1f2cd0
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2023-09-01 06:42:46 +0300

    added date_created to calls and consultations tables

--- ekseer_api/models.py
+++ ekseer_api/models.py
@@ -133,6 +133,8 @@ class Call(models.Model):
     patient_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE,null=True,blank=True)
     doctor_id = models.IntegerField(null=True,blank=True)
     is_new = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Consultation(models.Model):
     id = models.AutoField(primary_key=True)
@@ -145,6 +147,8 @@ class Consultation(models.Model):
     assessment = models.CharField(max_length=255, null=True, blank=True)
     medication = models.CharField(max_length=255, null=True, blank=True)
     sick_leave = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 
